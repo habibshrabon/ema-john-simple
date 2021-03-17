3cb060d1a9106f34f481069b4145d8ba95597993
habibshrabon <snhabib777@gmail.com>
2021-03-17 23:52:36 +0600

    work is done

--- src/components/Shipment/Shipment.js
+++ src/components/Shipment/Shipment.js
@@ -1,11 +1,33 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { useForm } from 'react-hook-form';
+import { UserContext } from '../../App';
+import '../Shipment/Shipment.css';
 
 const Shipment = () => {
-    return (
-        <div>
-            <h1>This is shipment</h1>
-        </div>
-    );
+    const { register, handleSubmit, watch, errors } = useForm();
+    const [loggedInUser, setLoggedInUser] = useContext(UserContext);
+    const onSubmit = data => {
+    console.log('form submitted', data);
+  }
+
+  console.log(watch("example")); // watch input value by passing the name of it
+
+  return (
+    <form onSubmit={handleSubmit(onSubmit)} className="ship-form"> 
+      <input name="name" defaultValue={loggedInUser.name} ref={register({ required: true })} placeholder="Your name" />
+      {errors.name && <span className="error">Name is required</span>}
+
+      <input name="email" defaultValue={loggedInUser.email} ref={register({ required: true })} placeholder="Your email" />
+      {errors.email && <span className="error">Email is required</span>}
+
+      <input name="address" ref={register({ required: true })} placeholder="Your address" />
+      {errors.address && <span className="error">address is required</span>}
+
+      <input name="phone" ref={register({ required: true })} placeholder="Your phone Number" />
+      {errors.phone && <span className="error">Phone Number is required</span>}
+      <input type="submit" />
+    </form>
+  );
 };
 
 export default Shipment;
